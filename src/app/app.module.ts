f8112523684860984470ed63f8ae2c054a6163ad
Eaton Wu <ew9170@rit.edu>
2021-12-17 18:39:38 -0500

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponentComponent } from './header-component/header-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponentComponent
   ],
   imports: [
     BrowserModule
